5ff8876ebece23f801873ea098a669dff872ccd8
ashwin.sankar@tagitmobile.com <ashwin.sankar@tagitmobile.com>
2020-05-08 19:43:23 +0530

    ASL : 20200508 - Query changes
    Fixed insert query
    Optimized querying formation process

--- src/modules/login/LoginQuery.js
+++ src/modules/login/LoginQuery.js
@@ -2,8 +2,8 @@ const connection = require('../../utility/Database/GetConnection')
 
 function getUserByNameAndType(details){
     return new Promise(function(resolve, reject) {
-        let query = `SELECT * from login_service_details where username = '${details.username}' and user_type = '${details.user_type}'`
-        connection.query(query, function (err, rows, fields) {
+        let query = `SELECT * from login_service_details where username = ? and user_type = ?`
+        connection.query(query,[details.username,details.user_type], function (err, rows, fields) {
         if (err) {
             return reject(err)
         }
--- src/modules/registration/RegistrationService.js
+++ src/modules/registration/RegistrationService.js
@@ -5,8 +5,8 @@ const pwd = require('../../utility/PasswordManager')
 async function  register(registrationDetails){
     try{
         if(await isUserNameEligibleForRegistration(registrationDetails.username)){
-            let details = buildRegistrationDetails(registrationDetails)
-            let affectedRows = await registrationQuery.saveUserDetails(details)
+            registrationDetails = buildRegistrationDetails(registrationDetails)
+            let affectedRows = await registrationQuery.saveUserDetails(registrationDetails)
             if(affectedRows == 1){
                 delete registrationDetails.password;
                 registrationDetails.registrationSuccesful = true
@@ -26,22 +26,11 @@ async function  register(registrationDetails){
 }
 
 function buildRegistrationDetails(registrationDetails){
-    let details = {}
-    details.email_id = registrationDetails.email_id || 'unknown@mycerverllo.com'
-    details.username = registrationDetails.username || 'Unknown'
-    details.password = pwd.encodePassword(registrationDetails.password || 'Password@123')
-    details.email_verified = registrationDetails.email_verified || false
-    details.created_by = registrationDetails.created_by || null
-    details.first_name = registrationDetails.first_name || null
-    details.last_name = registrationDetails.last_name || null
-    details.phone_number = registrationDetails.phone_number || null
-    details.designation_role = registrationDetails.designation_role || null
-    details.bio_description = registrationDetails.bio_description || null
-    details.profile_link = registrationDetails.profile_link || null
-    details.user_type = registrationDetails.user_type || null
-    details.coe = registrationDetails.coe || null
-    details.practice = registrationDetails.practice || null
-    return details
+    registrationDetails.password = pwd.encodePassword(registrationDetails.password)
+    registrationDetails.email_verified = registrationDetails.email_verified || 0
+    registrationDetails.created_by = registrationDetails.created_by || 'Admin'
+    registrationDetails.user_type = registrationDetails.user_type || 'User'
+    return registrationDetails
 }
 
 async function isUserNameEligibleForRegistration(userName){
